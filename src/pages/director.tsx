2b991ebd361f829d637b40b8c8ebe8715081ecb4
jaumVitor <joao.vitor.pires.060@gmail.com>
2024-09-05 12:02:37 -0300

    Feat: Create function handleSubmit to add new director

--- src/pages/director.tsx
+++ src/pages/director.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { Button } from '@/components/ui/button'
 import {
   Table,
@@ -32,7 +34,6 @@ import {
   SelectValue
 } from '@/components/ui/select'
 
-import * as React from 'react'
 import { CalendarIcon } from '@radix-ui/react-icons'
 import { format } from 'date-fns'
 
@@ -45,7 +46,48 @@ import {
 } from '@/components/ui/popover'
 
 export default function Director() {
-  const [date, setDate] = React.useState<Date>()
+  const [departaments] = useState<string[]>([
+    'Administração',
+    'Recursos Humanos',
+    'Financeiro',
+    'Marketing',
+    'Vendas'
+  ])
+
+  const [date, setDate] = useState<Date>()
+  const [name, setName] = useState<string>('')
+  const [selectedDepartment, setSelectedDepartment] = useState<string>('Vendas')
+  const [dataTable, setDataTable] = useState<{ 
+    id: number, 
+    name: string, 
+    department: string, 
+    hireDate: Date | null 
+  }[]>([]);
+  
+  function handleNameChangeName(event: React.ChangeEvent<HTMLInputElement>) {
+    setName(event.target.value)
+    console.log('Nome do diretor:', event.target.value)
+  }
+  
+  const handleSelectChange = (value: string) => {
+    setSelectedDepartment(value)
+    console.log('Departamento selecionado:', value)
+  }
+  
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    // Aqui você pode enviar os dados do formulário para a API 
+    
+    const newDataTable = {
+      id: dataTable.length + 1,
+      name: name,
+      department: selectedDepartment,
+      hireDate: date || null
+    }
+
+    setDataTable([...dataTable, newDataTable])
+    console.log('Formulário enviado')
+  }
 
   return (
     <div className="pt-5">
@@ -69,73 +111,84 @@ export default function Director() {
           </DialogTrigger>
           <DialogContent className="sm:max-w-[600px]">
             <DialogHeader>
-              <DialogTitle className='text-contrast text-2xl font-bold'>Cadastro de diretores</DialogTitle>
+              <DialogTitle className="text-contrast text-2xl font-bold">
+                Cadastro de diretores
+              </DialogTitle>
               <DialogDescription>
                 Nesse formulário você pode cadastrar um novo diretor na
                 plataforma.
               </DialogDescription>
             </DialogHeader>
-            <div className="grid gap-4 py-4">
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="name">Nome completo</Label>
-                <Input
-                  id="name"
-                  value="Nome do diretor"
-                  className="col-span-3"
-                />
+            <form onSubmit={handleSubmit}>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="name">Nome completo</Label>
+                  <Input
+                    id="name"
+                    value={name}
+                    onChange={handleNameChangeName}
+                    className="col-span-3"
+                    placeholder="Nome do diretor"
+                  />
+                  <Label htmlFor="department">Departamento</Label>
+                  {/* Opções de cargos cadastradas para diretores */}
+                  <Select onValueChange={handleSelectChange}>
+                    <SelectTrigger className="w-full col-span-2">
+                      <SelectValue placeholder="Selecione o departamento" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Departamentos cadastrados</SelectLabel>
+                        {/* <SelectItem value="apple">Apple</SelectItem> */}
+                        {departaments.map((departament, index) => {
+                          return (
+                            <SelectItem key={index} value={departament}>
+                              {departament}
+                            </SelectItem>
+                          )
+                        })}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
 
-                <Label htmlFor="department">Departamento</Label>
-                {/* Opções de cargos cadastradas para diretores */}
-                <Select>
-                  <SelectTrigger className="w-full col-span-2">
-                    <SelectValue placeholder="Selecione o departamento" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectGroup>
-                      <SelectLabel>Departamentos cadastrados</SelectLabel>
-                      {/* <SelectItem value="apple">Apple</SelectItem> */}
-                      {dataTable.map(data => {
-                        return (
-                          <SelectItem key={data.id} value={data.department}>
-                            {data.department}
-                          </SelectItem>
-                        )
-                      })}
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
-                
-                <br />
-                <Label htmlFor="hireDate">Data de contratação</Label>
-                <div className='col-span-2'>
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <Button
-                        variant={'outline'}
-                        className={cn(
-                          'w-[267px] justify-start text-left font-normal',
-                          !date && 'bg-violet-10'
-                        )}
-                      >
-                        <CalendarIcon className="mr-2 h-4 w-4" />
-                        {date ? format(date, 'PPP') : <span>Escolha a data</span>}
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0" align="start">
-                      <Calendar
-                        mode="single"
-                        selected={date}
-                        onSelect={setDate}
-                        initialFocus
-                      />
-                    </PopoverContent>
-                  </Popover>
+                  <br />
+                  <Label htmlFor="hireDate">Data de contratação</Label>
+                  <div className="col-span-2">
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant={'outline'}
+                          className={cn(
+                            'w-[267px] justify-start text-left font-normal',
+                            !date && 'bg-violet-10'
+                          )}
+                        >
+                          <CalendarIcon className="mr-2 h-4 w-4" />
+                          {date ? (
+                            format(date, 'PPP')
+                          ) : (
+                            <span>Escolha a data</span>
+                          )}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                          mode="single"
+                          selected={date}
+                          onSelect={setDate}
+                          initialFocus
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </div>
                 </div>
               </div>
-            </div>
-            <DialogFooter>
-              <Button className='w-full mt-5' type="submit">Salvar cadastro</Button>
-            </DialogFooter>
+              <DialogFooter>
+                <Button className="w-full mt-5" type="submit">
+                  Salvar cadastro
+                </Button>
+              </DialogFooter>
+            </form>
           </DialogContent>
         </Dialog>
       </div>
@@ -154,7 +207,7 @@ export default function Director() {
               <TableCell className="font-medium">{data.id}</TableCell>
               <TableCell>{data.name}</TableCell>
               <TableCell>{data.department}</TableCell>
-              <TableCell className="text-right">{data.hireDate}</TableCell>
+              <TableCell className="text-right">{data.hireDate ? data.hireDate.toString() : null}</TableCell>
             </TableRow>
           ))}
         </TableBody>
--- src/pages/mainLayout.tsx
+++ src/pages/mainLayout.tsx
@@ -3,13 +3,12 @@ import Header from '../components/header';
 import Wrapper from '../components/Wrapper';
 import { BackgroundGradientAnimation } from '@/components/ui/background-gradient-animation';
 
-
 export default function MainLayout() {
   return (
     <div className="h-screen flex flex-col">
       <Header />
       <div className="flex h-full overflow-hidden">
-        <BackgroundGradientAnimation className="absolute top-0 left-0 h-full w-screen">
+        <BackgroundGradientAnimation className="absolute top-0 left-0 h-screen w-screen">
           <main className="flex flex-col w-[100%] h-screen pointer-events-auto bg-zinc-100 z-10">
             <Wrapper>
               <Outlet /> {/* Renderiza as rotas internas, como Home */}
